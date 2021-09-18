1308ff3e16a1fd958481423f300fb4c1b277ada7
hamaa-affix <hamaagive1108@gmail.com>
2021-09-18 22:59:01 +0900

    演算子について

--- main.go
+++ main.go
@@ -2,67 +2,49 @@ package main
 
 import "fmt"
 
-//変数
-
-func outer() {
-		s4 := "outer"
-		fmt.Println(s4)
-}
-
+//演算
 func main() {
-		//明示的な定義
-		//var 変数名 型 = 値
-		var i int = 100
-		var s string = "hello Go"
-		var t,f bool = true, false
-		//異なる型で定義する場合
-		var (
-			i2 int = 200
-			s2 string = "hello"
-		)
-		//変数だけ定義する
-		var i3 int
-		var s3 string
+	fmt.Println(1 + 2) //3
 
-		i3 = 300
-		s3 = "GO"
+	fmt.Println(1 - 2) //-1
 
-		//再代入
-		i = 150
+	fmt.Println(5 * 2) //10
 
-		fmt.Println(i)
-		fmt.Println(s)
-		fmt.Println(t, f)
-		fmt.Println(i2, s2)
-		fmt.Println(i3, s3)
+	fmt.Println(30 / 2) //15
 
-		//暗黙的な定義(型宣言をしなくても型注釈してくれる)
-		//変数名 := 値
-		i4 := 400
-		fmt.Println(i4)
-		//再代入も可能
-		i4 = 500
-		fmt.Println(i4)
-		//再定義はできない
-		// i4 :=500
+	fmt.Println(9 % 4) //1
+	//文字列結合
+	fmt.Println("ABC" + "def") //ABCdef
 
-		//一度定義された型は再代入時に型を変えることはできない
-		//i4 = "stringへ" //これはできない。int型で宣言指定いるのでint型の値しか入れれない
-		outer()
+	n := 0
+	n += 4 //n = n + 4
+	fmt.Println(n)
+	n ++ //n = n + 1
+	n -- //n = n -1
 
-   /*
-				int8	8bit	符号あり	-128	127
-				int16	16bit	符号あり	-32768	32767
-				int32	32bit	符号あり	-2147483648	2147483647
-				int64	64bit	符号あり	-9223372036854775808	9223372036854775807
-				uint8(byte)	8	符号なし	0	255
-				uint16	16bit	符号なし	0	65535
-				uint32	32bit	符号なし	0	4294967295
-				uint64	64bit	符号なし	0	1844674407370955161
-				環境依存(よしなにマシーンのスペックに合わせて、bit列を合わしてくれる)、明示的にbitを記載してもいいし、記載しなくても良い
-	 */
-	  var int1 int = 100
-		//var int2 int64 = 200
+	s := "ABCD"
+	s += "ef" //s = s + "ef"
+	fmt.Println(s)
 
-		fmt.Println(int1 + 50) //150
+	//比較演算子
+	fmt.Println(1 == 1) //true
+
+	fmt.Println(1 == 2) //false
+
+	fmt.Println(4 <= 4) //true
+
+	fmt.Println(4 >= 6) //false
+
+	fmt.Println(4 < 4) //false
+
+	fmt.Println(true != false) //true
+
+	//論理演算子
+	fmt.Println(true && (false == true)) //false
+
+	fmt.Println(true || false == true) //true
+
+	fmt.Print(!true) //false
+
+	fmt.Print(!true == true) //false
 }
