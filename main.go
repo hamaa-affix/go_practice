acd63b5784de0fef11b24d3fe85e7e486fe2c3c8
hamaa-affix <hamaagive1108@gmail.com>
2021-09-26 00:04:10 +0900

    関数について

--- main.go
+++ main.go
@@ -2,43 +2,59 @@ package main
 
 import "fmt"
 
-//関数
-func Plus(x int, y int) int {
-	return x + y
-}
-//複数戻り値がある場合
-func Div(x int, y int) (int, int) {
-	plus := x + y
-	minus := x - y
-	return plus, minus
+func ReturnFunc() func() {
+	return func ()  {
+		fmt.Println("I am fucntion")
+	}
 }
 
-//戻り値の変数を指定することができる
-//return の値が省略されているが、戻り値で変数をしているので省略することができる
-func Double(price int) (result int) {
-	result = price  * 2
-	return
+func CallFunction(f func()) {
+	f()
 }
 
-//返り値がない型
-func NoReturn() {
-	fmt.Println("no return")
-	return
-}
+ //クロージャー
+  func Later() func(string) string {
+		var store string
+		return func (next string) string {
+				s := store
+				store = next
+				return s
+		}
+	}
+
+	//ジェネレーター なんらかのルールをもとに連続した値を返す。go ではジェネレーターはないがクロージャーで実装可能
+	func integers() func () int {
+		i := 0
+		return func() int {
+			  i ++
+				return i
+		}
+	}
 
 func main() {
-	i := Plus(2, 4)
-	fmt.Println(i)//6
-	fmt.Println(Plus(1, 4)) //5
 
-	i2, i3 := Div(3, 2)
-	fmt.Println(i2, i3)
+	//無記名関数 クロージャー
+	f := func (x, y int) int {
+			return x + y
+	}
+	i := f(1, 2)
+	fmt.Println(i)
 
-	i4, _ := Div(4, 5) //変数をアンダーバーで定義すると戻り値を破棄することができる
-	fmt.Println(i4)
+	//関数を返す関数
+	f2 := ReturnFunc()
+	f2()
 
-	i5 := Double(300)
-	fmt.Println(i5)
+	//引数に関数をとる。クロージャ的
+	CallFunction(func ()  {
+		fmt.Println("Iam argu")
+	})
 
-	NoReturn()
+	f4 := Later()
+	fmt.Println(f4("hello"))
+
+	f5 := integers()
+	fmt.Println(f5())
+	fmt.Println(f5())
+	fmt.Println(f5())
+	fmt.Println(f5())
 }
