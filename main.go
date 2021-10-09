cda6a5d807526e1eb6eb6da7bf5a3a4dc98350fa
hamaa-affix <hamaagive1108@gmail.com>
2021-10-09 10:52:06 +0900

    平行処理のdemo

--- main.go
+++ main.go
@@ -1,60 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func ReturnFunc() func() {
-	return func ()  {
-		fmt.Println("I am fucntion")
+func sub() {
+	for {
+		fmt.Println("sub loop")
+		time.Sleep(100 * time.Microsecond)
 	}
 }
-
-func CallFunction(f func()) {
-	f()
-}
-
- //クロージャー
-  func Later() func(string) string {
-		var store string
-		return func (next string) string {
-				s := store
-				store = next
-				return s
-		}
-	}
-
-	//ジェネレーター なんらかのルールをもとに連続した値を返す。go ではジェネレーターはないがクロージャーで実装可能
-	func integers() func () int {
-		i := 0
-		return func() int {
-			  i ++
-				return i
-		}
-	}
 
 func main() {
+	go sub()
 
-	//無記名関数 クロージャー
-	f := func (x, y int) int {
-			return x + y
+	for {
+		fmt.Println("main loop")
+		time.Sleep(200 * time.Microsecond)
 	}
-	i := f(1, 2)
-	fmt.Println(i)
-
-	//関数を返す関数
-	f2 := ReturnFunc()
-	f2()
-
-	//引数に関数をとる。クロージャ的
-	CallFunction(func ()  {
-		fmt.Println("Iam argu")
-	})
-
-	f4 := Later()
-	fmt.Println(f4("hello"))
-
-	f5 := integers()
-	fmt.Println(f5())
-	fmt.Println(f5())
-	fmt.Println(f5())
-	fmt.Println(f5())
 }
