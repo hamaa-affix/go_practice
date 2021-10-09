7911be5be08c319e73b9792f43aeae8f47164f48
hamaa-affix <hamaagive1108@gmail.com>
2021-10-09 21:43:22 +0900

    demo map

--- main.go
+++ main.go
@@ -2,59 +2,25 @@ package main
 
 import "fmt"
 
-func ReturnFunc() func() {
-	return func ()  {
-		fmt.Println("I am fucntion")
-	}
-}
-
-func CallFunction(f func()) {
-	f()
-}
-
- //クロージャー
-  func Later() func(string) string {
-		var store string
-		return func (next string) string {
-				s := store
-				store = next
-				return s
-		}
-	}
-
-	//ジェネレーター なんらかのルールをもとに連続した値を返す。go ではジェネレーターはないがクロージャーで実装可能
-	func integers() func () int {
-		i := 0
-		return func() int {
-			  i ++
-				return i
-		}
-	}
-
 func main() {
+	var m = map[string]int{"A": 100, "B": 200}
+	fmt.Println(m)
 
-	//無記名関数 クロージャー
-	f := func (x, y int) int {
-			return x + y
+	m1 := map[string]int{"B": 300, "C": 400}
+	fmt.Println(m1)
+
+	m2 := make(map[int]string)
+	fmt.Println(m2)
+	m2[1] = "JAPAN"
+	m2[2] = "USA"
+	fmt.Println(m2)
+
+	_, isValue := m2[3]
+	if !isValue {
+		 fmt.Println("error")
 	}
-	i := f(1, 2)
-	fmt.Println(i)
 
-	//関数を返す関数
-	f2 := ReturnFunc()
-	f2()
-
-	//引数に関数をとる。クロージャ的
-	CallFunction(func ()  {
-		fmt.Println("Iam argu")
-	})
-
-	f4 := Later()
-	fmt.Println(f4("hello"))
-
-	f5 := integers()
-	fmt.Println(f5())
-	fmt.Println(f5())
-	fmt.Println(f5())
-	fmt.Println(f5())
+	for i, v := range m2 {
+		fmt.Println(i, v)
+	}
 }
