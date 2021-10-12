89657fe6407a198b54e2be4c3ec86dc6ecf6e046
hamaa-affix <hamaagive1108@gmail.com>
2021-10-12 23:24:33 +0900

    struct demo

--- main.go
+++ main.go
@@ -19,6 +19,17 @@ type T struct {
 	Job
 }
 
+	//constructer
+func NewUser(name string, age int) *User  {
+	return &User{
+		Name: name,
+		Age: age,
+	}
+}
+
+//スライスを定義し代入する
+type Users []*User
+
 func intUser1(user User) {
 	user.Name = "user1です"
 	user.Age = 25
@@ -87,4 +98,45 @@ func main() {
 		},
 	}
 	fmt.Println(user7.Job.Name)
+
+	//constructer demo
+	user8 := NewUser("user8", 10)
+	fmt.Println(user8)
+	fmt.Println(user8.Name)
+
+	user9 := User{"a", 1}
+	user10 := User{"a", 1}
+	user11 := User{"a", 1}
+
+	//sliceへ格納する
+	users := Users{}
+	//ポインターの値を代入しないといけない
+	users = append(users, &user9, &user10, &user11)
+	fmt.Println(users)
+	//forで回すときは配列、スライス、マップでないとだめ
+	for _, u := range users {
+		fmt.Println(*u)
+	}
+
+	//mapで定義する
+	m := map[int]User{
+		1: {Name: "b", Age: 2},
+		2: {Name: "c", Age: 3},
+	}
+
+	for _, i := range m {
+		fmt.Println(i)
+	}
+	//mapのkeyにUser型を定義することも可能
+	m2 := map[User]string {
+		{Name: "d", Age: 10}: "tokyo",
+		{Name: "e", Age: 12}: "USA",
+	}
+
+	for i, m := range m2 {
+		if i.Name == "d" {
+			continue
+		}
+		fmt.Println(m)
+	}
 }
