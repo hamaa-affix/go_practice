4fef6f3aeafb271bebdb5e0425db971b9e224896
hamaa-affix <hamaagive1108@gmail.com>
2021-10-22 16:08:01 +0900

    test demo

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"newmod/alib"
 	"newmod/foo"
 )
+
+//test
+func IsOne(i int) bool {
+	if i == 1 {
+		return true
+	} else {
+		return false
+	}
+}
 
 //package  のインポート
 func main() {
 		fmt.Println(foo.Max)
+		fmt.Println(foo.ReturnMin())
+
+		s := []int{1, 2, 3, 4, 5}
+		fmt.Println(alib.Average(s))
 }
