d17fdd1b7ea69cef6efe58716c3a2cc6bd543a0a
hamaa-affix <hamaagive1108@gmail.com>
2021-10-24 12:53:07 +0900

    log出力demo

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"newmod/alib"
 	"newmod/foo"
+	"log"
+	"os"
 )
 
 //test
@@ -15,6 +17,8 @@ func IsOne(i int) bool {
 	}
 }
 
+//logの出力
+
 //package  のインポート
 func main() {
 		fmt.Println(foo.Max)
@@ -22,4 +26,19 @@ func main() {
 
 		s := []int{1, 2, 3, 4, 5}
 		fmt.Println(alib.Average(s))
+
+		//標準出力を行う
+		log.SetOutput(os.Stdout)//logの出力先を標準出力に変える
+		log.Print("Log\n")
+		log.Println("Log2")
+		log.Printf("Log%d\n", 3)
+
+		//任意のファイルにログを出力させる
+		f, err := os.Create("test.log")
+		if err != nil {
+			return
+		}
+		//作成したio.Writer型のファイルを出力先に設定する
+		log.SetOutput(f)
+		log.Println("ファイルに書き込む")
 }
