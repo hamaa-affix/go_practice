3c0c1436e292d77a5655e267ea5c306764703155
hamaa-affix <hamaagive1108@gmail.com>
2021-10-31 21:11:29 +0900

    server demo

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"newmod/alib"
-	"newmod/foo"
-	"log"
-	"os"
-	"sort"
+	// "newmod/alib"
+	// "newmod/foo"
+	// "log"
+	// "os"
+	// "sort"
+	"net/http"
 )
 
 //test
@@ -24,49 +25,60 @@ type Entry struct {
 }
 //logの出力
 
+
+//server
+type Myhandler struct{}
+
+func (h *Myhandler) ServeHTTP(w http.ResponseWriter, r *http.Request ) {
+		fmt.Fprintf(w, "hello world")
+}
+
 //package  のインポート
 func main() {
-		fmt.Println(foo.Max)
-		fmt.Println(foo.ReturnMin())
+		// fmt.Println(foo.Max)
+		// fmt.Println(foo.ReturnMin())
 
-		s := []int{1, 2, 3, 4, 5}
-		fmt.Println(alib.Average(s))
+		// s := []int{1, 2, 3, 4, 5}
+		// fmt.Println(alib.Average(s))
 
-		//標準出力を行う
-		log.SetOutput(os.Stdout)//logの出力先を標準出力に変える
-		log.Print("Log\n")
-		log.Println("Log2")
-		log.Printf("Log%d\n", 3)
+		// //標準出力を行う
+		// log.SetOutput(os.Stdout)//logの出力先を標準出力に変える
+		// log.Print("Log\n")
+		// log.Println("Log2")
+		// log.Printf("Log%d\n", 3)
 
-		//任意のファイルにログを出力させる
-		f, err := os.Create("test.log")
-		if err != nil {
-			return
-		}
-		//作成したio.Writer型のファイルを出力先に設定する
-		log.SetOutput(f)
-		log.Println("ファイルに書き込む")
+		// //任意のファイルにログを出力させる
+		// f, err := os.Create("test.log")
+		// if err != nil {
+		// 	return
+		// }
+		// //作成したio.Writer型のファイルを出力先に設定する
+		// log.SetOutput(f)
+		// log.Println("ファイルに書き込む")
 
-		//sort
-		i := []int{2, 4 ,5, 1, 3}
-		sort.Ints(i)
-		fmt.Println(i)
+		// //sort
+		// i := []int{2, 4 ,5, 1, 3}
+		// sort.Ints(i)
+		// fmt.Println(i)
 
-		string := []string{"a", "y", "u", "b"}
-		sort.Strings(string)
-		fmt.Println(string)
+		// string := []string{"a", "y", "u", "b"}
+		// sort.Strings(string)
+		// fmt.Println(string)
 
-		//sliceテーブル
-		el := []Entry{
-			{"A", 20},
-			{"t", 30},
-			{"b", 40},
-			{"c", 50},
-			{"t", 20},
-			{"a", 40},
-		}
+		// //sliceテーブル
+		// el := []Entry{
+		// 	{"A", 20},
+		// 	{"t", 30},
+		// 	{"b", 40},
+		// 	{"c", 50},
+		// 	{"t", 20},
+		// 	{"a", 40},
+		// }
 
-		fmt.Println(el)
-		sort.Slice(el, func(i, j int) bool { return el[i].Name < el[j].Name})
-		fmt.Println(el)
+		// fmt.Println(el)
+		// sort.Slice(el, func(i, j int) bool { return el[i].Name < el[j].Name})
+		// fmt.Println(el)
+
+		//serverとして立ち上げる
+		http.ListenAndServe(":8080", &Myhandler{})
 }
